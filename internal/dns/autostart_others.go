6430e21c4910e5df6f33e125f9720a9002e3c2ab
ducng99 <tom@tomng.dev>
2024-12-30 14:09:51 +1300

    feat(dns): Added message when running autostart on non-Windows

--- internal/dns/autostart_others.go
+++ internal/dns/autostart_others.go
@@ -3,6 +3,9 @@
 
 package dns
 
+import "github.com/ducng99/gohole/internal/logger"
+
 func RegisterAutostart() error {
+	logger.Printf(logger.LogWarn, "Autostart command is only supported on Windows at the moment.\nYou have to manually set up autostarting gohole on Linux or MacOS.\n")
 	return nil
 }
