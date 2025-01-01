1b0ad4857cb19d67e855b4e2c7ec40d72eec68c7
ducng99 <tom@ducng.dev>
2025-01-01 18:51:33 +1300

    refactor: use `windows` module instead of using syscall

--- internal/hosts/hosts_path_windows.go
+++ internal/hosts/hosts_path_windows.go
@@ -5,27 +5,17 @@ package hosts
 
 import (
 	"path/filepath"
-	"syscall"
-	"unsafe"
 
 	"github.com/ducng99/gohole/internal/logger"
+	"golang.org/x/sys/windows"
 )
 
 func getHostsFilePath() (string, error) {
-	kernel32 := syscall.NewLazyDLL("kernel32.dll")
-	getWindowsDirectoryHandle := kernel32.NewProc("GetWindowsDirectoryA")
-
-	windowsDir := make([]byte, 260)
-
-	bytesWritten, _, err := getWindowsDirectoryHandle.Call(uintptr(unsafe.Pointer(&windowsDir[0])), 260)
-	if bytesWritten == 0 {
+	windowsDir, err := windows.GetWindowsDirectory()
+	if err != nil {
 		logger.Printf(logger.LogError, "Cannot get Windows directory\n")
 		return "", err
 	}
 
-	if uint32(bytesWritten) == 260 {
-		logger.Printf(logger.LogWarn, "The Windows directory path has reached maximum length. This could be an error.\n")
-	}
-
-	return filepath.Join(string(windowsDir[:bytesWritten]), "System32", "drivers", "etc", "hosts"), nil
+	return filepath.Join(windowsDir, "System32", "drivers", "etc", "hosts"), nil
 }
